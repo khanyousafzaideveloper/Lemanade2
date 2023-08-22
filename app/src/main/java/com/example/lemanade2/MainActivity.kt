bd609e2c68aa51cd5c09ee7715acd42b6c5f90e7
Ahmad <khanyousafzaideveloper@gmail.com>
2023-08-22 09:27:16 +0500

    Appbar added to Lemonade app

--- app/src/main/java/com/example/lemanade2/MainActivity.kt
+++ app/src/main/java/com/example/lemanade2/MainActivity.kt
@@ -17,11 +17,13 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,10 +32,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,6 +64,7 @@ fun LemonadeApp() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LemonadeTutorial(modifier: Modifier= Modifier.wrapContentSize(Alignment.Center)) {
     var image by remember {
@@ -73,8 +76,8 @@ fun LemonadeTutorial(modifier: Modifier= Modifier.wrapContentSize(Alignment.Cent
         R.drawable.lemon_drink -> R.string.drink
         else -> R.string.start_again
     }
-    val contentDesc = when(textResource){
-        R.string.tap_the_lemon_tree_to_select_a_lemon-> R.string.Lemon_tree
+    val contentDesc = when (textResource) {
+        R.string.tap_the_lemon_tree_to_select_a_lemon -> R.string.Lemon_tree
         R.string.Keep_tapping_the_lemon_to_squeeze_it -> R.string.Lemon
         R.string.drink -> R.string.Glass_of_lemanade
         else -> R.string.start_again
@@ -82,52 +85,75 @@ fun LemonadeTutorial(modifier: Modifier= Modifier.wrapContentSize(Alignment.Cent
     var times = (2..4).random()
 
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Box(modifier = Modifier.size(190.dp).background(Color.Gray) .border(4.dp, Color(105, 205, 216), RoundedCornerShape(4.dp)))
-        {
-            Button(
-                onClick = {
-                    image = when (image) {
-                        R.drawable.lemon_tree -> R.drawable.lemon_squeeze
-                        R.drawable.lemon_squeeze -> {
-                            times--
-                            if(times!=0){
-                                R.drawable.lemon_squeeze
-                            }
-                        else{
-                            R.drawable.lemon_drink
-                        }}
-                        R.drawable.lemon_drink -> R.drawable.lemon_restart
-                        else -> R.drawable.lemon_tree
-                    }
-                },
-                modifier = Modifier.fillMaxSize(),
-                shape = RoundedCornerShape(dimensionResource(androidx.core.R.dimen.compat_control_corner_material)),
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
-            ) {
-                Image(
-                    painter = painterResource(id = image),
-                    contentDescription = contentDesc.toString(),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(8.dp)
-                )
-            }
-        }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = {
+                Text("Lemonade", fontWeight = FontWeight.Bold)
 
-        Spacer(modifier = Modifier.padding(16.dp))
-        Text(
-            text = stringResource(id = textResource),
-            fontSize = 18.sp,
-            style = MaterialTheme.typography.bodyLarge
-        )
+            },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.Yellow,
+                    titleContentColor = Color.Black
+                ))
+        }
+    )
+    {
+            contentPadding ->
+
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(contentPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .background(Color.Transparent)
+                    .border(4.dp, Color(105, 205, 216), RoundedCornerShape(4.dp))
+            )
+            {
+                Button(
+                    onClick = {
+                        image = when (image) {
+                            R.drawable.lemon_tree -> R.drawable.lemon_squeeze
+                            R.drawable.lemon_squeeze -> {
+                                times--
+                                if (times != 0) {
+                                    R.drawable.lemon_squeeze
+                                } else {
+                                    R.drawable.lemon_drink
+                                }
+                            }
+
+                            R.drawable.lemon_drink -> R.drawable.lemon_restart
+                            else -> R.drawable.lemon_tree
+                        }
+                    },
+                    Modifier.wrapContentSize(),
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(122, 255, 243, 255))
+                ) {
+                    Image(
+                        painter = painterResource(id = image),
+                        contentDescription = contentDesc.toString(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(
+                text = stringResource(id = textResource),
+                fontSize = 18.sp,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
